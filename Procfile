df0f358a14299a40f0f10e407bce1fb565f70d8a
athul-narayanan <athulnarayanan62@gmail.com>
2025-05-03 17:58:50 -0400

    CORS Issue fixed

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn dashboardapi.wsgi:application
--- dashboardapi/settings.py
+++ dashboardapi/settings.py
@@ -35,10 +35,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'dashboard'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -87,7 +89,7 @@ ALLOWED_HOSTS = ['data-north-production.up.railway.app', "127.0.0.1"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000", 
-    "https://data-north-dashboard.vercel.app/"
+    "https://data-north-dashboard.vercel.app"
 ]
 
 
